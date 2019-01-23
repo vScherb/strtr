b2f3d25a1790184abaf6cb0957dd8c886fb48564
Christian Kernstock <c.kernstock@me.com>
2019-01-23 22:28:16 +0100

    change api to localStorage

--- src/client/api.ts
+++ src/client/api.ts
@@ -1,61 +1,62 @@
 import { BlogApi, BlogPost } from './types';
 
-const api: BlogApi = {
+function wait(ms: number = 750)
+{
+  return new Promise(resolve =>
+  {
+    setTimeout(() =>
+    {
+      resolve();
+    }, ms);
+  });
+};
+
+export const api: BlogApi = {
   getAllBlogPosts(): Promise<BlogPost[]>
   {
-    return fetch('/ajax/posts', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(response => response.json());
+    const keys = Object.keys(localStorage);
+    const items = keys.map(key => JSON.parse(localStorage.getItem(key))).reverse();
+
+    return wait(300).then(() => items);
   },
   getSingleBlogPost(id: number): Promise<BlogPost>
   {
-    return fetch('/ajax/posts/' + id, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(response => response.json());
+    const item = JSON.parse(localStorage.getItem(id + ''));
+
+    return wait(200).then(() => item);
+  },
+  updateBlogPost(post: BlogPost): Promise<Response>
+  {
+    localStorage.setItem(post.id + '', JSON.stringify(post));
+
+    return wait(250).then(() => null);
   },
 
   addBlogPost(post: BlogPost): Promise<Response>
   {
-    return fetch('/ajax/posts', {
-      method: 'PUT',
-      body: JSON.stringify({
-        post
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  },
-  updateBlogPost(post: BlogPost): Promise<Response>
-  {
-    return fetch('/ajax/posts/' + post.id, {
-      method: 'POST',
-      body: JSON.stringify({
-        post
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  },
+    localStorage.setItem(post.id + '', JSON.stringify(post));
 
+    return wait(500).then(() => null);
+  },
   getPostsByTags(tags: string[]): Promise<BlogPost[]>
   {
-    return fetch('/ajax/posts/by-tags', {
-      method: 'POST',
-      body: JSON.stringify({
-        tags
-      }),
-      headers: {
-        'Content-Type': 'application/json'
+    const allItems: BlogPost[] = Object
+      .keys(localStorage)
+      .map(key => JSON.parse(localStorage.getItem(key)));
+
+    const filtered = allItems.filter(item =>
+    {
+      let result = true;
+
+      for (const tag of tags)
+      {
+        result = result && item.tags.indexOf(tag) > -1;
       }
-    }).then(response => response.json());
+
+      return result;
+    });
+
+    return wait(400).then(() => filtered);
   }
 };
 
