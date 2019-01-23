1ee955cceea1d8a3954aa4774fb5e7e59c183c70
Christian Kernstock <c.kernstock@me.com>
2019-01-23 22:30:01 +0100

    remove ajax router

--- src/node/www/router/index.js
+++ src/node/www/router/index.js
@@ -12,8 +12,6 @@ router.use('/', express.static('dist'));
 
 router.use(bodyParser.json());
 
-router.use('/ajax', require('./ajax'));
-
 router.use((request, response) =>
 {
   response.sendFile(path.join(path.dirname(require.main.filename), 'dist/index.html'));
