8fdc819aba39f014a236189eef79e6bae4f82dbf
Kernstock <C.Kernstock@ipi-gmbh.com>
2019-01-24 10:25:18 +0100

    puush

--- src/client/layout/App.tsx
+++ src/client/layout/App.tsx
@@ -1,9 +1,9 @@
 import { AppHeader, AppSidebar, AppFooter } from './';
-
-import { BlogPostItem } from '../components';
+import { IndexPage } from '../pages';
 
 // 3rd party
 import * as React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 export function App()
 {
@@ -14,7 +14,10 @@ export function App()
       <div className="row">
         <div className="col-md-8 blog-main">
 
-          <BlogPostItem />
+          <Switch>
+            <Route exact path='/' component={IndexPage} />
+            <Route exact path='/new' render={() => <div>new page</div>} />
+          </Switch>
 
         </div>
 
---
+++ src/client/pages/IndexPage.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export class IndexPage extends React.Component<{}, {}>
+{
+  constructor(props: any)
+  {
+    super(props);
+  }
+
+  render()
+  {
+    return <div>das ist meine indexpage.tsx</div>
+  }
+};
