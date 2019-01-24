91d80125c3ce8827795b9f36264887694ece9e3a
Kernstock <C.Kernstock@ipi-gmbh.com>
2019-01-24 13:29:55 +0100

    edit post form

--- src/client/components/EditPostForm.tsx
+++ src/client/components/EditPostForm.tsx
@@ -1,11 +1,51 @@
 import * as React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export class EditPostForm extends React.Component<{}, {}>
+export interface EditPostFormState
+{
+  titleTextboxValue: string;
+  contentTextboxValue: string;
+  tagsTextboxValue: string;
+};
+
+export class EditPostForm extends React.Component<{}, EditPostFormState>
 {
   constructor(props: any)
   {
     super(props);
+
+    this.state = {
+      titleTextboxValue: 'mein title',
+      contentTextboxValue: 'toller content',
+      tagsTextboxValue: 'foor, bar'
+    };
+  }
+
+  private onTitleTextboxChange(e: React.ChangeEvent)
+  {
+    const { value } = (e.target as HTMLInputElement);
+
+    this.setState({
+      titleTextboxValue: value
+    });
+  }
+
+  private onContentTextboxChange(e: React.ChangeEvent)
+  {
+    const { value } = (e.target as HTMLTextAreaElement);
+
+    this.setState({
+      contentTextboxValue: value
+    });
+  }
+
+  private onTagsTextboxChange(e: React.ChangeEvent)
+  {
+    const { value } = (e.target as HTMLInputElement);
+
+    this.setState({
+      tagsTextboxValue: value
+    });
   }
 
   render()
@@ -13,15 +53,15 @@ export class EditPostForm extends React.Component<{}, {}>
     return <>
       <div className="row mt-3">
         <div className="col-md-12 mb-3">
-          <input type="text" className="form-control form-control-lg" placeholder="Title..." />
+          <input type="text" className="form-control form-control-lg" placeholder="Title..." value={this.state.titleTextboxValue} onChange={this.onTitleTextboxChange.bind(this)} />
         </div>
 
         <div className="col-md-12 mb-3">
-          <textarea className="form-control blog-content-textarea" placeholder="Blogtext..."></textarea>
+          <textarea className="form-control blog-content-textarea" placeholder="Blogtext..." value={this.state.contentTextboxValue} onChange={this.onContentTextboxChange.bind(this)}></textarea>
         </div>
 
         <div className="col-md-12 mb-3">
-          <input type="text" className="form-control" placeholder="Tags..." />
+          <input type="text" className="form-control" placeholder="Tags..." value={this.state.tagsTextboxValue} onChange={this.onTagsTextboxChange.bind(this)} />
         </div>
       </div>
 
